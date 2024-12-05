ed0f57cca842df8fe7dd8f8d1d5c069a6f1e9c14
JsFn99 <fnine2015@gmail.com>
2024-12-05 22:09:42 +0100

    Speed Sliders

--- sketch.js
+++ sketch.js
@@ -8,6 +8,9 @@ let zombieImage;
 let manImage;
 let heroImage;
 let rockImage;
+let sliderVitesseMaxZombies;
+let sliderVitesseMaxMen;
+let sliderVitesseMaxHero;
 
 function preload() {
   bgImage = loadImage('assets/desert.png');
@@ -27,6 +30,11 @@ function setup() {
   }
 
   obstacles.push(new Obstacle(width / 2, height / 2, 100, "green"));
+
+  // Création des sliders
+  createMonSlider("Vitesse Zombies", 1, 10, 3, 0.1, 20, 0, "white", "maxSpeed", zombies);
+  createMonSlider("Vitesse Men", 1, 10, 5, 0.1, 20, 40, "white", "maxSpeed", men);
+  createMonSlider("Vitesse Hero", 1, 10, 5, 0.1, 20, 80, "white", "maxSpeed", [hero]);
 }
 
 function draw() {
@@ -58,10 +66,6 @@ function draw() {
   });
 }
 
-function mousePressed() {
-  obstacles.push(new Obstacle(mouseX, mouseY, random(20, 80), "green"));
-}
-
 function keyPressed() {
   console.log(key);
   if (key === 'm') {
@@ -73,4 +77,36 @@ function keyPressed() {
   if (key === 'z') {
     zombies.push(new Zombie(random(width), random(height)));
   }
+  if (key === 'o') {
+    obstacles.push(new Obstacle(mouseX, mouseY, random(20, 80), "green"));
+  }
 }
+
+function createMonSlider(label, min, max, val, step, x, y, color, prop, targetArray) {
+  // On crée un slider pour régler la vitesse max
+  // des véhicules
+  // slider les paramètres : Min, Max, Valeur, Pas
+  let slider = createSlider(min, max, val, step);
+  // on positionne le slider en haut à gauche du canvas
+  slider.position(100, y + 17);
+  // Label à gauche du slider "maxSpeed"
+  let labelHTML = createP(label);
+  // label en blanc
+  labelHTML.style('color', 'white');
+  // on le positionne en x=10 y = 10
+  labelHTML.position(x, y);
+  //on affiche la valeur du slider à droite du slider
+  let labelValue = createP(slider.value());
+  labelValue.style('color', color);
+  labelValue.position(250, y);
+  // on veut que la valeur soit mise à jour quand on déplace
+  // le slider
+  slider.input(() => {
+    labelValue.html(slider.value());
+    // On change la propriété de tous les characters
+    targetArray.forEach(item => {
+      item[prop] = slider.value();
+    });
+  });
+}
+
